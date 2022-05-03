e479cce79dfdc45216aa46b26a85d11db64abb9b
Denny Spears <spearden@protonmail.com>
2022-05-03 03:23:38 +0300

    add json data

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
+let data = require('./data.json');
+const fs = require('fs');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
     console.log('Server is listening!');
@@ -14,16 +17,28 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/actors', (req, res) => {
+    const actors = data.Actors;
+    console.log(actors);
+    res.render('actors', { actors });
+});
+
 app.get('/actors/:actor', (req, res) => {
-    console.log(req.params);
-    res.send('actors!');
+    
 });
 
 app.get('*', (req, res) => {
     res.render('404');
 });
 
-app.post('/', (req, res) => {
-    console.log('1', req.query);
+app.post('/data', (req, res) => {
+    console.log(req.body);
+    data.Actors.push(req.body);
+    console.log(data);
+    console.log(JSON.stringify(data));
+    fs.writeFileSync('./data.json', JSON.stringify(data), (err) => {
+        if (err) throw err;
+    });
+    res.render('index');
 });
 
